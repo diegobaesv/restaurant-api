ac62667d99f094852e5ccc1768f0d9144a035c08
Diego Baes <diegobaesv@gmail.com>
2024-09-09 20:59:38 -0500

    sucural entity

---
+++ src/main/java/com/sise/restaurant_api/entities/Sucursal.java
@@ -0,0 +1,56 @@
+package com.sise.restaurant_api.entities;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "sucursales")
+public class Sucursal {
+
+    @Id 
+    @Column(name = "id_sucursal")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idSucursal;
+
+    @Column(name = "nombre")
+    private String nombre;
+
+    @Column(name = "direccion")
+    private String direccion;
+    
+    @Column(name = "imagen_url")
+    private String imagenUrl;
+
+    @Column(name = "telefono")
+    private String telefono;
+
+    @Column(name = "correo")
+    private String correo;
+
+    @Column(name = "dias_atencion")
+    private String diasAtencion;
+
+    @Column(name = "horario_atencion")
+    private String horarioAtencion;
+
+    @Column(name = "latitud")
+    private String latitud;
+
+    @Column(name = "longitud")
+    private String longitud;
+
+    @Column(name = "estado_auditoria")
+    private String estadoAuditoria;
+
+    @Column(name = "fecha_creacion")
+    private LocalDateTime fechaCreacion;
+}
