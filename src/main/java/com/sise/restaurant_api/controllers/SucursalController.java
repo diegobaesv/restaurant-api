9d9b57a5a2e17f0725de018f871d5cf6874f5d40
Diego Baes <diegobaesv@gmail.com>
2024-09-13 20:58:55 -0500

    insertar y actualizar

--- src/main/java/com/sise/restaurant_api/controllers/SucursalController.java
+++ src/main/java/com/sise/restaurant_api/controllers/SucursalController.java
@@ -10,6 +10,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+
+
+
 
 @RestController
 @RequestMapping("/sucursales")
@@ -27,5 +35,39 @@ public class SucursalController {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
+
+
+    @GetMapping("/{idSucursal}")
+    public ResponseEntity<Sucursal> obtenerSucursal(@PathVariable Integer idSucursal) {
+        try {
+            Sucursal sucursal = sucursalService.obtenerSucursal(idSucursal);
+            return new ResponseEntity<>(sucursal, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PostMapping("")
+    public ResponseEntity<Sucursal> insertarSucursal(@RequestBody Sucursal sucursalInsert) {
+        try {
+            Sucursal sucursal = sucursalService.insertarSucursal(sucursalInsert);
+            return new ResponseEntity<>(sucursal, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PutMapping("/{idSucursal}")
+    public ResponseEntity<Sucursal> actualizarSucursal(@PathVariable Integer idSucursal, @RequestBody Sucursal sucursalUpdate) {
+        try {
+            sucursalUpdate.setIdSucursal(idSucursal);
+            Sucursal sucursal = sucursalService.actualizarSucursal(sucursalUpdate);
+            return new ResponseEntity<>(sucursal, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    
+    
 
 }
